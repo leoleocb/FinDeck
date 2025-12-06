ce70d98740343df688fa710f7aa23a336ed2fd83
leoleocb <ldch07@outlook.com>
2025-12-06 03:14:35 -0500

    Initial Commit

---
+++ FinDeck/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FinDeck
+//
+//  Created by Leandro Coba Huayas on 6/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
